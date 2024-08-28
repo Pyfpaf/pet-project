0289c56068dbf3db5f020a73c8515ff3066c94a8
Alex <bocandzo@mail.ru>
2024-08-28 21:31:27 +0300

    comlited autogpbl parser

--- parsers/autogpbl.py
+++ parsers/autogpbl.py
@@ -2,7 +2,9 @@ import requests
 import json
 import csv
 import os
+from datetime import datetime
 from bs4 import BeautifulSoup
+import time
 
 url = 'https://auction.autogpbl.ru/auction?page='
 
@@ -10,6 +12,9 @@ headers = {
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/png,image/svg+xml,*/*;q=0.8',
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:129.0) Gecko/20100101 Firefox/129.0'
 }
+
+path = os.environ.get('PROJECT_PATH', '.')
+cards_item = []
 
 
 def get_data():
@@ -19,66 +24,116 @@ def get_data():
     # with open('data/index.html', 'w', encoding='utf-8') as file:
     #     file.write(resp.text)
 
-    with open('data/index.html', encoding='utf-8') as file:
-        src = file.read()
+    # with open('data/index.html', encoding='utf-8') as file:
+    #     src = file.read()
 
-    soup = BeautifulSoup(src, 'lxml')
-    cards = soup.find_all('article', class_='css-zt7hdf')
-
-    for card in cards:
-
-        full_item = (
-            card.find('div', class_='css-u8gsze').find('div', class_='typography default css-1m5714k').text.strip())
-
-        fail_word = ['рицеп', 'ватор', 'грузчик', 'станов', 'реза', 'аток', 'кран', 'Кран', 'асси',
-                     'втобус', 'еларус', 'ашинострои', 'усоровоз', 'дозер', 'тилифт', 'рактор',
-                     'омбайн', 'рейдер', 'уидор', 'орвардер']
-
-        if any(word in full_item for word in fail_word):
-            continue
-
-        factory = full_item.replace(',', '').split()[0].lower()
-        model = (full_item.replace(',', '').replace('(Shaanxi)', '').replace('(Lada) ', '').replace('*', '').split()[
-                     1].lower())
-        year = int(full_item.split()[-1])
-        city = \
-        card.find('div', class_='css-u8gsze').find('div', class_='typography default css-uukhpx').text.strip().split()[
-            0].lower()
-        price = int(
-            card.find('div', class_='css-u8gsze').find('div', class_='primary-text css-1ctn9oc').text.strip().replace(
-                ' ', '').replace('₽', ''))
-
-        try:
-            mileage_full = \
-            card.find('div', class_='css-u8gsze').find_all('div', class_='typography default css-bdji1y')[0].text
-
-            if 'км' in mileage_full:
-                mileage = mileage_full.strip().replace(' ', '').replace(' км', '')
-            else:
-                mileage = None
-
-        except:
-            mileage = None
-
-        print(f'{factory} ## {model} ## {year} ## {city} ## {mileage} ## {price}')
-
-
-def get_page():
     s = requests.Session()
 
-    for i in [40, 41]:
-        resp = s.get(url=url + str(i), headers=headers)
+    page = 1
+
+    while True:
+        resp = s.get(url=url + str(page), headers=headers)
         soup = BeautifulSoup(resp.text, 'lxml')
         cards = soup.find_all('article', class_='css-zt7hdf')
 
-        try:
-            print(cards[0].find('div', class_='css-u8gsze').find('div', class_='typography default css-1m5714k').text.strip())
-        except:
-            print(f'страница {i} отсутствует на сайте')
+        if cards:
+
+            for card in cards:
+
+                full_item = (card.find('div', class_='css-u8gsze').find('div', class_='typography default css-1m5714k').text.strip())
+
+                fail_word = ['рицеп', 'ватор', 'грузчик', 'станов', 'реза', 'аток', 'кран', 'Кран', 'асси',
+                             'втобус', 'еларус', 'ашинострои', 'усоровоз', 'дозер', 'тилифт', 'рактор',
+                             'омбайн', 'рейдер', 'уидор', 'орвардер']
+
+                if any(word in full_item for word in fail_word):
+                    continue
+
+                factory = full_item.replace(',', '').split()[0].lower()
+                model = (full_item.replace(',', '').replace('(Shaanxi)', '').replace('(Lada) ', '').replace('*', '').split()[1].lower())
+
+                try:
+                    year = int(full_item.split()[-1])
+                except:
+                    year = None
+
+                try:
+                    city = card.find('div', class_='css-u8gsze').find('div', class_='typography default css-uukhpx').text.strip().split()[0].lower()
+                except:
+                    city = None
+
+                try:
+                    price = int(card.find('div', class_='css-u8gsze').find('div', class_='primary-text css-1ctn9oc').text.strip().replace(' ', '').replace('₽', ''))
+                except:
+                    price = None
+
+                try:
+                    mileage_full = \
+                    card.find('div', class_='css-u8gsze').find_all('div', class_='typography default css-bdji1y')[0].text
+
+                    if 'км' in mileage_full:
+                        mileage = mileage_full.strip().replace(' ', '').replace(' км', '')
+                    else:
+                        mileage = None
+
+                except:
+                    mileage = None
+
+                cards_item.append(
+                    {
+                        'factory': factory,
+                        'model': model,
+                        'year': year,
+                        'city': city,
+                        'mileage': mileage,
+                        'price': price
+                    }
+                )
+
+            print(f'[+] Обработалась страница {page}')
+            time.sleep(1)
+
+            page += 1
+
+        else:
+            print(f'Страница {page - 1} была последней на сайте')
+            break
 
 
 def main():
-    get_page()
+
+    print(f'Старт работы парсинга сайта Газпром Автолизинг')
+    get_data()
+    with open(f'{path}/data/gpbl_data_{datetime.now().strftime("%Y%m%d")}.json', 'w', encoding='utf-8') as file:
+        json.dump(cards_item, file, indent=4, ensure_ascii=False)
+
+    with open(f'{path}/data/gpbl_data_{datetime.now().strftime("%Y%m%d")}.csv', 'w', encoding='utf-8', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(
+            (
+                'factory',
+                'model',
+                'year',
+                'city',
+                'mileage',
+                'price'
+            )
+        )
+
+    for item in cards_item:
+        with open(f'{path}/data/gpbl_data_{datetime.now().strftime("%Y%m%d")}.csv', 'a', encoding='utf-8', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(
+                (
+                    item['factory'],
+                    item['model'],
+                    item['year'],
+                    item['city'],
+                    item['mileage'],
+                    item['price']
+                )
+            )
+    print(f'Окончание работы парсинга сайта Газпром Автолизинг')
 
 
 if __name__ == '__main__':
